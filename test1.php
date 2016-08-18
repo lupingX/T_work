68b8737e57da83de4d282fc117577c08c4af56ef
lupingX <xiangluping93@hotmail.com>
2016-08-18 18:39:38 +0100

    almost done

--- test1.php
+++ test1.php
@@ -1,28 +1,31 @@
+
 <?php
 $mysqliObj =  new mysqli("localhost", "root", "",'db1');	//连接数据库系统
 
 
 $mysqliObj->query("set name utf8");
+$mysqliObj->query("use db1");
 $sql1 = "show databases;";
+
 $result = $mysqliObj->query($sql1);
 if($result === false){
-	echo "执行失败，请参考："  .  mysql_error();
+	echo "Failure："  .  mysql_error();
 }
 else{
 	//此时，$result就是“结果集”（数据集）；
-	echo "执行成功！数据如下：";
-	$rec = $result->fetch_row();
-	echo "<br />";
-	//echo $result;
-	// while( $rec = mysql_fetch_array( $result )){
-	// //mysql_fetch_array()会取出该结果集中的“一行数据”，并取得该行数据后赋值给$rec；
-	// //此$rec就是一个数组，其下标就是字段名；
-	// //在此while循环中，mysql_fetch_array()会一次次（一行行）取出结果集中的所有数据；
-	// //然后，在这里就可以处理该数组$rec了：
-		// echo "<br />f1：" . $rec[0];
-	// 	echo "————f2：" . $rec['name'];
-	// 	echo "————f3：" . $rec['age'];
-	// }
+	echo "Success：";
+
+	while ( $rec = $result->fetch_row()) {
+	
+	// echo "<br />".$rec[0];
+	}
+	$sql1 = "create table tab1(id int auto_increment primary key,
+								FileName varchar(100) unique key,
+								folder_id int);";
+	$sql2 = "create table tab2(id int auto_increment primary key,
+							 folder varchar(100) unique key);";
+	$mysqliObj->query($sql1);	
+	$mysqliObj->query($sql2);					 
 }
 
 
@@ -56,22 +59,62 @@ else{
 	);
 
 
-function get_array_elems($arrResult, $where="C:\ "){
- while(list($key,$value)=each($arrResult)){
-    if (is_array($value)){
-      get_array_elems($value, $where."$key\ ");
+function get_array_elems($arrResult, $where="C: "){
+	global $mysqliObj;
+ 	while(list($key,$value)=each($arrResult)){
+    	if (is_array($value)){
+      	get_array_elems($value, $where."$key ");
     }
-    else {
-      for ($i=0; $i<count($value);$i++){
-      echo $where.$value."<BR>\n";
+    	else {
+      	for ($i=0; $i<count($value);$i++){
+      	// echo $where.$value."<BR>\n";
       // inset sql by using $where and $value
       // $a= $where;
       // echo $a;
-      
-      }
+      $sql3="INSERT INTO `tab2`( `folder`) VALUES ('$where')";
+      $mysqliObj->query($sql3);
+      $sql4= "SELECT `id` FROM `tab2` WHERE folder='$where'";
+      $result = $mysqliObj->query($sql4);
+
+      $rec = $result->fetch_row();
+      // echo "<BR>\n".$rec[0];
+      $sql5= "INSERT INTO `tab1`(`FileName`, `folder_id`) VALUES ('$value',' $rec[0]')";
+      $mysqliObj->query($sql5);
+      // $sql4= "INSERT INTO `tab1`(`FileName`, `folder_id`) VALUES ([value-1],[value-2],[value-3])"
+      	}
     }
  }
 }
- get_array_elems($file_system);
+function search_Sql($content){
+	$edit_content = '%'.$content.'%';
+	// echo $edit_content;
+	global $mysqliObj;
+	$sql = "SELECT `tab1`.`FileName`, `tab2`.`folder` FROM `tab1` LEFT JOIN `tab2` on `tab1`.`folder_id`=`tab2`.`id` WHERE `tab1`.`FileName` Like '$edit_content' ";
+	$i=0;
+	if($result = $mysqliObj->query($sql)){		
+		while ( $rec = $result->fetch_row()) {
+			$Result_sql[$i]=$rec[1].$rec[0];
+			echo "<BR>\n".$Result_sql[$i];
+			$i++;
+			
+		}
+	}
+	$sql = "SELECT `tab2`.`folder` FROM `tab2` WHERE `tab2`.`folder` LIKE '$edit_content' ";
+	if($result = $mysqliObj->query($sql)){		
+		while ( $rec = $result->fetch_row()) {
+			$Result_sql[$i]=$rec[0];
+			echo "<BR>\n".$Result_sql[$i];
+			$i++;
+			
+		}
+	}
 
+}
+ // get_array_elems($file_system);
+	// $sql3="INSERT INTO `tab2`( `folder`) VALUES ($where)";
+ //      $result = $mysqliObj->query($sql1);
+ $content=$_POST['data1'];
+ print_r($_POST);
+ echo $content;
+ search_Sql($content);
 ?>
